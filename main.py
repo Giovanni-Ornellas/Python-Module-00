2ae4fcaac845c781f724eea2c48d5ef9c9d9ded5
Giovani Ornellas <giovanniafco@gmail.com>
2026-05-16 18:55:32 -0300

    ADD: main file

---
+++ main.py
@@ -0,0 +1,18 @@
+from ex1.ft_garden_name import ft_garden_name
+from ex2.ft_plot_area import ft_plot_area
+from ex3.ft_harvest_total import ft_harvest_total
+from ex4.ft_plant_age import ft_plant_age
+from ex5.ft_water_reminder import ft_water_reminder
+from ex6.ft_count_harvest_iterative import ft_count_harvest_iterative
+from ex6.ft_count_harvest_recursive import ft_count_harvest_recursive
+from ex7.ft_seed_inventory import ft_seed_inventory
+
+if __name__ == "__main__":
+    ft_garden_name()
+    ft_plot_area()
+    ft_harvest_total()
+    ft_plant_age()
+    ft_water_reminder()
+    ft_count_harvest_iterative()
+    ft_count_harvest_recursive()
+    ft_seed_inventory()
